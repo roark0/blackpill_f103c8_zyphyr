f891543392a7c5e3a784504219ccf09196a7d55d
lordyung <lordyung@duck.com>
2025-11-19 10:09:55 +0800

    stm32_min_dev.overlay led0 ok
    Signed-off-by: lordyung <lordyung@duck.com>

--- src/main.c
+++ src/main.c
@@ -1,83 +1,50 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
-
-/* 定义UART设备 */
-// #define USART_DEVICE_NAME DT_LABEL(DT_NODELABEL(usart1))
-// #define UART_DEVICE_NODE DT_NODELABEL(usart1)
-
-/* 定义GPIO引脚PA11用于blinky */
 
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 #define SLEEP_TIME_MS 1000
 
-
 int main(void)
 {
     int ret;
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led))
+    printk("Starting LED blinky application\n");
+
+    if (!device_is_ready(led.port))
     {
+        printk("GPIO device not ready\n");
         return 0;
     }
-    // gpio_pin_set(&led, 0, led_state);
+
+    printk("GPIO device is ready\n");
+    printk("GPIO port: %p, pin: %d, flags: %d\n", led.port, led.pin, led.dt_flags);
 
     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     if (ret < 0)
     {
+        printk("Failed to configure GPIO pin (err %d)\n", ret);
         return 0;
     }
 
-#if 0
-    printk("UART1 and GPIO PA11 initialized successfully\n");
+    printk("GPIO pin configured successfully\n");
 
-    // 发送一些调试信息通过UART
-    uart_poll_out(uart_dev, 'H');
-    uart_poll_out(uart_dev, 'e');
-    uart_poll_out(uart_dev, 'l');
-    uart_poll_out(uart_dev, 'l');
-    uart_poll_out(uart_dev, 'o');
-    uart_poll_out(uart_dev, ' ');
-    uart_poll_out(uart_dev, 'U');
-    uart_poll_out(uart_dev, 'A');
-    uart_poll_out(uart_dev, 'R');
-    uart_poll_out(uart_dev, 'T');
-    uart_poll_out(uart_dev, '1');
-    uart_poll_out(uart_dev, ' ');
-    uart_poll_out(uart_dev, 'a');
-    uart_poll_out(uart_dev, 'n');
-    uart_poll_out(uart_dev, 'd');
-    uart_poll_out(uart_dev, ' ');
-    uart_poll_out(uart_dev, 'G');
-    uart_poll_out(uart_dev, 'P');
-    uart_poll_out(uart_dev, 'I');
-    uart_poll_out(uart_dev, 'O');
-    uart_poll_out(uart_dev, ' ');
-    uart_poll_out(uart_dev, 'P');
-    uart_poll_out(uart_dev, 'A');
-    uart_poll_out(uart_dev, '1');
-    uart_poll_out(uart_dev, '1');
-    uart_poll_out(uart_dev, '\r');
-    uart_poll_out(uart_dev, '\n');
-#endif
-
-    // 闪烁LED的循环 - 使用PA11引脚
-
+    // 个闪烁LED的循环
     while (1)
     {
         ret = gpio_pin_toggle_dt(&led);
         if (ret < 0)
         {
+            printk("Failed to toggle GPIO pin (err %d)\n", ret);
             return 0;
         }
 
+        printk("LED toggled, state: %s\n", led_state ? "ON" : "OFF");
         led_state = !led_state;
-        // printf("LED state: %s\n", led_state ? "ON" : "OFF");
         k_msleep(SLEEP_TIME_MS);
     }
 
